454c781e7de0ef1beb0c4ac22e93820e0d6acf69
TakingJester766 <zimmermanalex13@gmail.com>
2023-01-13 10:44:23 -0500

    working on styling for /application

--- src/components/NewProject/NewProject.jsx
+++ src/components/NewProject/NewProject.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Input, Button } from 'antd';
+import { useState } from 'react';
 
 const NewProject = () => {
-  return (
-    <div>
-        <br />
-        <Input id='url-search' placeholder="Page URL" />
-        <Button type="primary" htmlType="submit" className="submit-url">Upload</Button>
-        <br />
-    </div>
-  )
+
+    const [url, setUrl] = useState('');
+
+    const handleChange = (e) => {
+        setUrl(e.target.value);
+    }
+
+
+    return (
+        <div>
+            <br />
+            <Input id='url-search' placeholder="Page URL" />
+            <Button type="primary" htmlType="submit" className="submit-url" onChange={setUrl}>Upload</Button>
+            <br />
+        </div>
+    )
 }
 
 export default NewProject
--- src/pages/application/ProjectView/Application.jsx
+++ src/pages/application/ProjectView/Application.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import PersistentModal from '../../../components/ProjectModal/PersistentModal';
 import NewProject from '../../../components/NewProject/NewProject';
+import { Input, Button } from 'antd';
 
 const Contact = () => {
   return (
     <div>
+      <div className='top-container'>
+        <h1 className='project-header'>New Project</h1>
+        <Input id='url-search' placeholder="Page URL" />
+      </div>
       <div className='container'>
-          <h1 className='project-header'>New Project</h1>
-          <br />
+        <div className="left-side">
+        </div>
+        <div className="right-side">
+          <PersistentModal />
+        </div>
           
-          <NewProject />
+          
           {/*<PersistentModal />*/}
       </div>
     </div>
